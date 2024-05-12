e8d15db3a2c410ea0255099ff302fdb09a5a2c10
SANTIAGO FIERRO <santimarcelofierrod@gmail.com>
2024-05-11 19:53:58 -0500

    Update CSRF_TRUSTED_ORIGINS to use new domain for reconocimiento-numeros-cheques

--- reconocimiento_numeros/settings.py
+++ reconocimiento_numeros/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CSRF_TRUSTED_ORIGINS = ['https://reconocimiento-numeros.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://reconocimiento-numeros-cheques.onrender.com']
 
 
 
